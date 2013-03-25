dfe2c2b6e758875e1379a5f8b9e2887dfb9be56d
NoahShen <Noah.Shen87@gmail.com>
2013-03-25 16:25:18 +0800

    add other method

--- src/aria2rpc/aria2rpc.go
+++ src/aria2rpc/aria2rpc.go
@@ -1,7 +1,11 @@
 package aria2rpc
 
 import (
+	"bufio"
+	"encoding/base64"
 	"github.com/kdar/httprpc"
+	"io/ioutil"
+	"os"
 )
 
 const (
@@ -27,6 +31,91 @@ func AddUri(uri string, params map[string]string) (string, error) {
 	return replyGID, nil
 }
 
+func AddTorrent(path string) (string, error) {
+	file, openFileErr := os.Open(path)
+	if openFileErr != nil {
+		return "", nil
+	}
+
+	fileBytes, readError := ioutil.ReadAll(bufio.NewReader(file))
+	if readError != nil {
+		return "", readError
+	}
+	coder := base64.StdEncoding
+	encoded := coder.EncodeToString(fileBytes)
+
+	method := "aria2.addTorrent"
+	paramArr := make([]interface{}, 1)
+	paramArr[0] = encoded
+	var replyGID string
+	err := httprpc.CallJson(RPC_VERSION, RPC_URL, method, paramArr, &replyGID)
+	if err != nil {
+		return "", err
+	}
+	return replyGID, nil
+}
+
+func Remove(gid string, force bool) (string, error) {
+	var method string
+	if force {
+		method = "aria2.forceRemove"
+	} else {
+		method = "aria2.remove"
+	}
+	paramArr := make([]interface{}, 1)
+	paramArr[0] = gid
+	var replyGID string
+	err := httprpc.CallJson(RPC_VERSION, RPC_URL, method, paramArr, &replyGID)
+	if err != nil {
+		return "", err
+	}
+	return replyGID, nil
+}
+
+func Pause(gid string, force bool) (string, error) {
+	var method string
+	if force {
+		method = "aria2.forcePause"
+	} else {
+		method = "aria2.pause"
+	}
+	paramArr := make([]interface{}, 1)
+	paramArr[0] = gid
+	var replyGID string
+	err := httprpc.CallJson(RPC_VERSION, RPC_URL, method, paramArr, &replyGID)
+	if err != nil {
+		return "", err
+	}
+	return replyGID, nil
+}
+
+func Unpause(gid string) (string, error) {
+	method := "aria2.unpause"
+	paramArr := make([]interface{}, 1)
+	paramArr[0] = gid
+	var replyGID string
+	err := httprpc.CallJson(RPC_VERSION, RPC_URL, method, paramArr, &replyGID)
+	if err != nil {
+		return "", err
+	}
+	return replyGID, nil
+}
+
+func GetStatus(gid string, keys []string) (map[string]interface{}, error) {
+	method := "aria2.tellStatus"
+	paramArr := make([]interface{}, 1)
+	paramArr[0] = gid
+	if keys != nil && len(keys) > 0 {
+		paramArr = append(paramArr, keys)
+	}
+	var reply = make(map[string]interface{})
+	err := httprpc.CallJson(RPC_VERSION, RPC_URL, method, paramArr, &reply)
+	if err != nil {
+		return nil, err
+	}
+	return reply, nil
+}
+
 func GetActive(keys []string) ([]map[string]interface{}, error) {
 	method := "aria2.tellActive"
 	paramArr := make([]interface{}, 0)
@@ -34,6 +123,75 @@ func GetActive(keys []string) ([]map[string]interface{}, error) {
 		paramArr = append(paramArr, keys)
 	}
 	var reply = make([]map[string]interface{}, 10)
+	err := httprpc.CallJson(RPC_VERSION, RPC_URL, method, paramArr, &reply)
+	if err != nil {
+		return nil, err
+	}
+	return reply, nil
+}
+
+func GetWaiting(offset, num int, keys []string) ([]map[string]interface{}, error) {
+	method := "aria2.tellWaiting"
+	paramArr := make([]interface{}, 2)
+	paramArr[0] = offset
+	paramArr[1] = num
+	if keys != nil && len(keys) > 0 {
+		paramArr = append(paramArr, keys)
+	}
+	var reply = make([]map[string]interface{}, 10)
+	err := httprpc.CallJson(RPC_VERSION, RPC_URL, method, paramArr, &reply)
+	if err != nil {
+		return nil, err
+	}
+	return reply, nil
+}
+
+func GetStopped(offset, num int, keys []string) ([]map[string]interface{}, error) {
+	method := "aria2.tellStopped"
+	paramArr := make([]interface{}, 2)
+	paramArr[0] = offset
+	paramArr[1] = num
+	if keys != nil && len(keys) > 0 {
+		paramArr = append(paramArr, keys)
+	}
+	var reply = make([]map[string]interface{}, 10)
+	err := httprpc.CallJson(RPC_VERSION, RPC_URL, method, paramArr, &reply)
+	if err != nil {
+		return nil, err
+	}
+	return reply, nil
+}
+
+func GetOption(gid string) (map[string]interface{}, error) {
+	method := "aria2.getOption"
+	paramArr := make([]interface{}, 1)
+	paramArr[0] = gid
+	var reply = make(map[string]interface{})
+	err := httprpc.CallJson(RPC_VERSION, RPC_URL, method, paramArr, &reply)
+	if err != nil {
+		return nil, err
+	}
+	return reply, nil
+}
+
+func ChangeOption(gid string, params map[string]string) (string, error) {
+	method := "aria2.changeOption"
+	paramArr := make([]interface{}, 1)
+	paramArr[0] = gid
+	paramArr = append(paramArr, params)
+
+	var reply string
+	err := httprpc.CallJson(RPC_VERSION, RPC_URL, method, paramArr, &reply)
+	if err != nil {
+		return "", err
+	}
+	return reply, nil
+}
+
+func GetGlobalStat() (map[string]interface{}, error) {
+	method := "aria2.getGlobalStat"
+	paramArr := make([]interface{}, 0)
+	var reply = make(map[string]interface{})
 	err := httprpc.CallJson(RPC_VERSION, RPC_URL, method, paramArr, &reply)
 	if err != nil {
 		return nil, err
--- src/aria2rpc/aria2rpc_test.go
+++ src/aria2rpc/aria2rpc_test.go
@@ -18,17 +18,101 @@ func NoTestAddUri(t *testing.T) {
 	log.Println(gid)
 }
 
-func TestGetActive(t *testing.T) {
-	keys := []string{"gid", "status"}
-	reply, err := GetActive(keys)
+func NoTestAddTorrent(t *testing.T) {
+	gid, err := AddTorrent("/home/noah/DueWest.torrent")
 	if err != nil {
-		log.Fatal("GetActive", err)
+		log.Fatal("add Torrent error:", err)
+	}
+	log.Println(gid)
+}
+
+func NoTestRemove(t *testing.T) {
+	gid, err := Remove("12", true)
+	if err != nil {
+		log.Fatal("Remove error:", err)
+	}
+	log.Println(gid)
+}
+
+func NoTestPause(t *testing.T) {
+	gid, err := Pause("16", false)
+	if err != nil {
+		log.Fatal("Pause error:", err)
+	}
+	log.Println(gid)
+}
+
+func NoTestUnpause(t *testing.T) {
+	gid, err := Unpause("16")
+	if err != nil {
+		log.Fatal("Unpause error:", err)
+	}
+	log.Println(gid)
+}
+
+func TestGetStatus(t *testing.T) {
+	keys := []string{"gid", "status"}
+	reply, err := GetStatus("16", keys)
+	if err != nil {
+		log.Fatal("GetStatus", err)
 	}
 	log.Println(reply)
+}
+
+func NoTestGetActive(t *testing.T) {
+	//keys := []string{"gid", "status"}
+	//reply, err := GetActive(keys)
+	//if err != nil {
+	//	log.Fatal("GetActive", err)
+	//}
+	//log.Println(reply)
 
 	reply2, err2 := GetActive(nil)
 	if err2 != nil {
 		log.Fatal("GetActive", err2)
 	}
 	log.Println(reply2)
+}
+
+func TestGetWaiting(t *testing.T) {
+	keys := []string{"gid", "status"}
+	reply, err := GetWaiting(0, 10, keys)
+	if err != nil {
+		log.Fatal("Waiting error:", err)
+	}
+	log.Println(reply)
+}
+
+func NoTestGetStopped(t *testing.T) {
+	reply, err := GetStopped(0, 10, nil)
+	if err != nil {
+		log.Fatal("Waiting error:", err)
+	}
+	log.Println(reply)
+}
+
+func NoTestGetOption(t *testing.T) {
+	reply, err := GetOption("16")
+	if err != nil {
+		log.Fatal("GetOption error:", err)
+	}
+	log.Println(reply)
+}
+
+func NoTestChangeOption(t *testing.T) {
+	params := make(map[string]string)
+	params["max-download-limit"] = "7K"
+	reply, err := ChangeOption("16", params)
+	if err != nil {
+		log.Fatal("ChangeOption error:", err)
+	}
+	log.Println(reply)
+}
+
+func NoTestGetGlobalStat(t *testing.T) {
+	reply, err := GetGlobalStat()
+	if err != nil {
+		log.Fatal("GetGlobalStat error:", err)
+	}
+	log.Println(reply)
 }
