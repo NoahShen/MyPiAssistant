d0c5c30c49ecfd23658f54b5439033f306635cba
NoahShen <Noah.Shen87@gmail.com>
2013-06-13 14:22:00 +0800

    change air api which's data from USEmbassy

--- src/aqi/aqifetcher_test.go
+++ src/aqi/aqifetcher_test.go
@@ -7,19 +7,25 @@ import (
 )
 
 func _TestFetch(t *testing.T) {
+	Debug = true
 	aqi, err := FetchAqiFromWeb("shanghai")
 	if err != nil {
 		t.Fatal(err)
 	}
-	fmt.Printf("aqi: %d, time: %v, source: %d", aqi.Aqi, time.Unix(aqi.Time, 0), aqi.Datasource)
-	fmt.Println()
+	for _, a := range aqi {
+		fmt.Printf("aqi: %d, time: %v, source: %d", a.Aqi, time.Unix(a.Time, 0), a.Datasource)
+		fmt.Println()
+	}
 }
 
 func _TestFetchFromCN(t *testing.T) {
+	Debug = true
 	aqi, err := FetchAqiFromWeb("suzhou")
 	if err != nil {
 		t.Fatal(err)
 	}
-	fmt.Printf("aqi: %d, time: %v, source: %d", aqi.Aqi, time.Unix(aqi.Time, 0), aqi.Datasource)
-	fmt.Println()
+	for _, a := range aqi {
+		fmt.Printf("aqi: %d, time: %v, source: %d", a.Aqi, time.Unix(a.Time, 0), a.Datasource)
+		fmt.Println()
+	}
 }
